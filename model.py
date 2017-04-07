f0b894d56584c3586407362718f02b5319c21620
Richard Zhao <linuxzsc@gmail.com>
2017-04-06 22:49:33 -0700

    add crop
    Signed-off-by: Richard Zhao <linuxzsc@gmail.com>

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import numpy as np
 import keras.layers as layers
 import keras
 
+crop_x = 58
+
 def get_data(path):
     lines = []
     with open(path+"driving_log.csv") as f:
@@ -18,14 +20,14 @@ def get_data(path):
     steerings = []
     for line in lines:
         f_center = path + line[0]
-        images.append(plt.imread(f_center))
+        images.append(plt.imread(f_center)[crop_x:])
         steering = float(line[3])
         steerings.append(steering)
     return np.array(images),np.array(steerings)
 
 def get_model():
     model = keras.models.Sequential()
-    model.add(layers.InputLayer(input_shape=(160,320,3)))
+    model.add(layers.InputLayer(input_shape=(160-crop_x,320,3)))
     model.add(layers.Conv2D(6,(3,3), activation='relu'))
     model.add(layers.MaxPool2D())
     model.add(layers.Conv2D(16,(3,3), activation='relu'))
@@ -41,6 +43,7 @@ def get_model():
 
 if __name__ == '__main__':
     x,y = get_data('data/')
+    print(x.shape,y.shape)
     model = get_model()
-    model.fit(x, y, batch_size=256, epochs=20, validation_split=0.2, shuffle=True)
+    model.fit(x, y, batch_size=100, epochs=1, validation_split=0.2, shuffle=True)
     model.save('model.h5')
