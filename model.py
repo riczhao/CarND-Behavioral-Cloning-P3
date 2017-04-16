285d636156108e36c199eb84b1bf830432432619
Richard Zhao <linuxzsc@gmail.com>
2017-04-15 22:33:28 -0700

    add video and example pictures
    Signed-off-by: Richard Zhao <linuxzsc@gmail.com>

--- model.py
+++ model.py
@@ -3,34 +3,110 @@ import matplotlib.pyplot as plt
 import numpy as np
 import keras.layers as layers
 import keras
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import random
+import math
 
-crop_x = 58
+crop_top = 58
+crop_bottom = 30
+crop_x = crop_top + crop_bottom
 
-def get_data(path):
-    lines = []
-    with open(path+"driving_log.csv") as f:
-        reader = csv.reader(f)
-        title = True
-        for line in reader:
-            if title:
-                title = False
-                continue
-            lines.append(line)
-    images = []
-    steerings = []
-    for line in lines:
-        f_center = path + line[0]
-        img = plt.imread(f_center)[crop_x:]/255. - 0.5
-        images.append(img)
-        steering = float(line[3])
-        steerings.append(steering)
-    return np.array(images),np.array(steerings)
+class DataGenerator(object):
+    def __init__(self, path='data/'):
+        self.path = path
+        self.h_flip_rate = 0.5
+        self.noise_std = 0.
+        self.n_samples = 0
+        x,y = self.parse_path()
+        self.train_valid_split(x, y)
+        self.sample_showed = False
 
-def augment(x, y, batch_size):
-    datagen = keras.preprocessing.image.ImageDataGenerator(
-        horizontal_flip=True)
-    datagen.fit(x)
-    datagen.flow(x, y, batch_size, shuffle=True)
+    def parse_path(self):
+        ''' get x,y, x is picture path '''
+        lines = []
+        with open(self.path+"driving_log.csv") as f:
+            reader = csv.reader(f)
+            title = True
+            for line in reader:
+                if title:
+                    title = False
+                    continue
+                lines.append(line)
+        self.n_samples = len(lines)
+        x = []
+        y = []
+        for line in lines:
+            if abs(float(line[3])) < 0.05:
+                x.append(line[1]) # left
+                y.append(float(line[3])+0.3)
+                x.append(line[2]) #right
+                y.append(float(line[3])-0.3)
+            elif abs(float(line[3])) > 0.7:
+                x.append(line[0]) # center
+                y.append(float(line[3]))
+                x.append(line[0]) # center
+                x.append(line[0]) # center
+                y.append(float(line[3]))
+                y.append(float(line[3]))
+        y = np.array(y)
+        return x,y
+
+    def train_valid_split(self, x, y, valid_size=0.1):
+        self.x_train,self.x_val,self.y_train,self.y_val = \
+            train_test_split(x, y, test_size=valid_size)
+        self.n_train = len(self.x_train)
+        self.n_val = len(self.x_val)
+        
+    def shuffle_train(self):
+        shuffle(self.x_train, self.y_train)
+
+    def gen_batch_train(self, x_path, y):
+        x = []
+        for i in range(len(x_path)):
+            fn = x_path[i]
+            img = plt.imread(fn)[crop_top:-crop_bottom]/255.
+            h_flip = random.random() <= self.h_flip_rate
+            if h_flip:
+                img = np.flip(img,1)
+                y[i] = -y[i]
+            if self.noise_std:
+                img += np.random.normal(scale=self.noise_std,size=img.shape)
+            x.append(img)
+        return (np.array(x), y)
+    
+    def gen_batch_val(self, x_path, y):
+        x = []
+        for fn in x_path:
+            img = plt.imread(fn)[crop_top:-crop_bottom]/255.
+            x.append(img)
+        return (np.array(x), y)
+    
+    def show_samples(self):
+        self.shuffle_train()
+        x,y = self.gen_batch_train(self.x_train[0:12],
+                             self.y_train[0:12])
+        for i in range(12):
+            plt.subplot(4,3,i+1)
+            plt.imshow(x[i])
+            plt.title('y:'+str(y[i]))
+        plt.show()
+        print(len(self.x_train))
+ 
+ 
+    def train_flow(self, batch_size):
+        while True:
+            self.shuffle_train()
+            for idx in range(0,self.n_train,batch_size):
+               yield self.gen_batch_train(self.x_train[idx:idx+batch_size],
+                                          self.y_train[idx:idx+batch_size])
+    
+    def valid_flow(self, batch_size):
+        while True:
+            for idx in range(0, self.n_val, batch_size):
+                yield self.gen_batch_val(self.x_val[idx:idx+batch_size],
+                                         self.y_val[idx:idx+batch_size])
 
 def get_model():
     model = keras.models.Sequential()
@@ -45,13 +121,99 @@ def get_model():
     model.add(layers.Dense(1024,activation='relu'))
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(1))
+    model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
+
+    return model
+
+def get_nv_model():
+    model = keras.models.Sequential()
+    model.add(layers.InputLayer(input_shape=(160-crop_x,320,3))) # 102x320
+    #model.add(layers.GaussianNoise(0.1))
+    model.add(layers.Conv2D(24,(5,5), activation='relu')) # 98x316
+    model.add(layers.Dropout(0.4))
+    model.add(layers.MaxPool2D(padding='same')) # 49x158
+    model.add(layers.Conv2D(36,(5,5), activation='relu')) # 45x154
+    model.add(layers.MaxPool2D(padding='same')) # 23x77
+    model.add(layers.Conv2D(48,(5,5), activation='relu')) # 19x73
+    model.add(layers.Dropout(0.4))
+    model.add(layers.MaxPool2D(padding='same')) # 10x37
+    model.add(layers.Conv2D(64,(3,3), activation='relu')) # 8x35
+  #  model.add(layers.MaxPool2D(padding='same')) # 4x18
+    model.add(layers.Conv2D(64,(3,3), activation='relu')) # 2x16
+  #  model.add(layers.MaxPool2D(padding='same')) # 1x8
+    model.add(layers.Dropout(0.4))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1024,activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1))
     model.compile('adam', 'mse')
 
     return model
 
+def get_nvidia_model(input_shape):
+    model = keras.models.Sequential()
+    model.add(layers.GaussianNoise(0.1, input_shape=input_shape))
+    model.add(layers.Conv2D(24, 5, 5, activation='elu',
+                          subsample = (2,2),border_mode='valid',init = 'he_normal'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(36, 5, 5,  activation='elu',
+                          subsample = (2,2),border_mode='valid',init = 'he_normal'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(48, 5, 5,  activation='elu',
+                          subsample = (2,2),border_mode='valid',init = 'he_normal'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(64, 3, 3,  activation='elu',
+                          subsample = (1,1),border_mode='valid',init = 'he_normal'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(64, 3, 3,  activation='elu',
+                          subsample = (1,1),border_mode='valid',init = 'he_normal'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(1164,init='he_normal',activation='elu'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(100,init='he_normal',activation='elu'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(50,init='he_normal',activation='elu'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(10,init='he_normal',activation='elu'))
+    model.add(layers.Dense(1,init='he_normal'))
+    model.compile(optimizer=Adam(lr=1e-04), loss='mean_squared_error')
+    return model
+
 if __name__ == '__main__':
-    x,y = get_data('data/')
-    print(x.shape,y.shape)
-    model = get_model()
-    model.fit(x, y, batch_size=100, epochs=1, validation_split=0.2, shuffle=True)
-    model.save('model.h5')
+    model_name = 'lenet'
+    input_shape=(160-crop_x,320,3)
+    gen = DataGenerator(path='c:\data\\')
+    #for i in range(10):
+    #    gen.show_samples()
+    if model_name == 'nv':
+        model = get_nv_model()
+        batch_size = 32
+    elif model_name == 'lenet':
+        model = get_model()
+        batch_size = 100
+    else:
+        model = get_nvidia_model(input_shape)
+        batch_size = 32
+    try:
+        model.load_weights(model_name+'_weights.h5')
+    except:
+        pass
+    keras.utils.vis_utils.plot_model(model, to_file=model_name+'.png')
+    hist = model.fit_generator(gen.train_flow(batch_size), math.ceil(gen.n_train/batch_size)/21,
+                        epochs=20,
+                        validation_data=gen.valid_flow(batch_size),
+                        validation_steps=math.ceil(gen.n_val/batch_size),
+                        verbose=1)
+    #model.fit(x, y, batch_size=100, epochs=1, validation_split=0.2, shuffle=True)
+    model.save_weights(model_name+'_weights.h5', True)
+    model.save(model_name+'.h5')
+
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
--- writeup_template.md
+++ writeup_template.md
@@ -18,13 +18,14 @@ The goals / steps of this project are the following:
 
 [//]: # (Image References)
 
-[image1]: ./examples/placeholder.png "Model Visualization"
-[image2]: ./examples/placeholder.png "Grayscaling"
-[image3]: ./examples/placeholder_small.png "Recovery Image"
-[image4]: ./examples/placeholder_small.png "Recovery Image"
+[image1]: ./examples/lenet.png "Model Visualization"
+[image2]: ./examples/center.jpg "Grayscaling"
+[image3]: ./examples/left.jpg "Recovery Image"
+[image4]: ./examples/right.jpg "Recovery Image"
 [image5]: ./examples/placeholder_small.png "Recovery Image"
 [image6]: ./examples/placeholder_small.png "Normal Image"
 [image7]: ./examples/placeholder_small.png "Flipped Image"
+[image8]: ./examples/train.png
 
 ## Rubric Points
 ###Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
@@ -54,15 +55,13 @@ The model.py file contains the code for training and saving the convolution neur
 
 ####1. An appropriate model architecture has been employed
 
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
+I tried a few module, including lenet-5 and nvidia net. It turned out modified lenet already work.
 
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
+My model consists two sets of (3x3 conv2D, 2x2 subsmaple) layers, followed by 1024 dense layer and 1 output layer.
 
 ####2. Attempts to reduce overfitting in the model
 
-The model contains dropout layers in order to reduce overfitting (model.py lines 21). 
-
-The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
+See function get_model. I added a few dropout layers. The dense layer's with high drop rate, while conv2d layer's with low drop rate.
 
 ####3. Model parameter tuning
 
@@ -70,7 +69,7 @@ The model used an adam optimizer, so the learning rate was not tuned manually (m
 
 ####4. Appropriate training data
 
-Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road ... 
+Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road.
 
 For details about how I created the training data, see the next section. 
 
@@ -78,23 +77,23 @@ For details about how I created the training data, see the next section.
 
 ####1. Solution Design Approach
 
-The overall strategy for deriving a model architecture was to ...
+To drive the car, I only need to detect lane position and predict steering angle with how much the lane off the picture center. So I used two convolution layers.
 
-My first step was to use a convolution neural network model similar to the ... I thought this model might be appropriate because ...
+I collected over 30,000 samples. I used 10% of them as validation data, which is a bit low because I want to check validation loss more frequently. If I check valid loss for every whole epic, it'll take a lot of time. So I intentionally reduced epic steps.
 
-In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
-
-To combat the overfitting, I modified the model so that ...
-
-Then I ... 
-
-The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
+To combat the overfitting, I tried:
+- add dropout layers
+- add a gausian noise layer
+- flip the image and minus steering angle
+- reverse direction when collecting data
+- For center image, only keep the samples with high angle.
+- For low angle samples, also use left and right images, with +0.3, -0.3 angle change accordingly.
 
 At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
 
 ####2. Final Model Architecture
 
-The final model architecture (model.py lines 18-24) consisted of a convolution neural network with the following layers and layer sizes ...
+The final model architecture consisted of two convolution layers followed by two dense layers.
 
 Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
 
@@ -110,11 +109,10 @@ I then recorded the vehicle recovering from the left side and right sides of the
 
 ![alt text][image3]
 ![alt text][image4]
-![alt text][image5]
 
 Then I repeated this process on track two in order to get more data points.
 
-To augment the data sat, I also flipped images and angles thinking that this would ... For example, here is an image that has then been flipped:
+To augment the data sat, I also flipped images and angles thinking that this would hellp reduce overfitting. For example, here is an image that has then been flipped:
 
 ![alt text][image6]
 ![alt text][image7]
@@ -124,6 +122,7 @@ Etc ....
 After the collection process, I had X number of data points. I then preprocessed this data by ...
 
 
-I finally randomly shuffled the data set and put Y% of the data into a validation set. 
+I finally randomly shuffled the data set and put 10% of the data into a validation set. 
 
-I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was Z as evidenced by ... I used an adam optimizer so that manually training the learning rate wasn't necessary.
+I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was 20 as evidenced by bellow. I used an adam optimizer so that manually training the learning rate wasn't necessary.
+![alt text][image8]
